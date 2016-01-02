301f1ee09a1d420b6b94dc03ab52cfeb4928e417
Russell Froehlich <Russ@Company43.com>
2016-01-02 09:36:10 -0800

    added InterviewFun.UnitTests project

---
+++ src/InterviewFun.UnitTests/SortTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterviewFun.UnitTests
+{
+	[TestClass]
+	public class SortTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual(43, 43);
+		}
+	}
+}
