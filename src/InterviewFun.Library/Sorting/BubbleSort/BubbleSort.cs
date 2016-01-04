872f7e3c49363767f2f785557c7e8f79cffa3413
Russell Froehlich <Russ@Company43.com>
2016-01-03 16:39:09 -0800

    added BubbleSort stub

---
+++ src/InterviewFun.Library/Sorting/BubbleSort/BubbleSort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace InterviewFun.Sorting.BubbleSort
+{
+	static public class BubbleSort
+	{
+		static public IList<T> Sort<T>(IList<T> list)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
