2ee20501981ccd7343eb501650cc6f660b8634de
Russell Froehlich <Russ@Company43.com>
2016-01-08 16:22:45 -0800

    oops, added linked list files that should have been in previous commit

---
+++ src/InterviewFun.Library/LinkedList/LinkedList.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace InterviewFun.LinkedList
+{
+	/// <summary>
+	/// A generic singly-linked list.
+	/// </summary>
+	/// <remarks>
+	/// This is a class singly-linked list data structure with a <see cref="Head"/> property representing the
+	/// first element in the list.
+	/// </remarks>
+	public class LinkedList<T> : IEnumerator<T>, IEnumerator
+	{
+		#region fields
+		private bool _isReset; 
+		#endregion
+
+		#region constructors
+
+		/// <summary>
+		/// Creates an empty linked list.
+		/// </summary>
+		public LinkedList()
+		{
+			_isReset = true;
+		}
+
+		#endregion
+
+
+		#region private properties
+		/// <summary>
+		/// The "head" node containing the first element in the list.
+		/// </summary>
+		/// <remarks>If the list is empty, the head is null.</remarks>
+		private Node<T> HeadNode
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// The "current" node used for iterating over elements in the list and for inserting new elements
+		/// into the list.
+		/// </summary>
+		private Node<T> CurrentNode
+		{
+			get;
+			set;
+		}
+		#endregion
+
+		#region methods
+		/// <summary>
+		/// Insert an element into the list.
+		/// </summary>
+		/// <param name="value">The element to insert.</param>
+		/// <remarks>
+		/// If the list has just been reset or is empty, the element will be inserted at the beginning of the list.
+		/// Otherwise, the element will be inserted right after <see cref="Current"/>.
+		/// </remarks>
+		public void Insert(T value)
+		{
+			if(_isReset)
+			{
+				Debug.Assert(CurrentNode == null);
+
+				Node<T> headNode0 = HeadNode;
+
+				HeadNode = new Node<T>(value);
+				HeadNode.Next = headNode0;
+
+				_isReset = false;
+			}
+			else
+			{
+				Debug.Assert(HeadNode != null);
+				Debug.Assert(CurrentNode != null);
+
+				Node<T> currentNode0 = CurrentNode;
+
+				CurrentNode = new Node<T>(value);
+				CurrentNode.Next = currentNode0;
+			}
+		}
+		#endregion
+
+		#region object methods
+		/// <summary>
+		/// Returns the linked list as a comma-delimited string of values.
+		/// </summary>
+		/// <returns>The linked list as a comma-delimited string of values.</returns>
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			if(HeadNode != null)
+			{
+				sb.Append(HeadNode.ToString());
+
+				Node<T> next = HeadNode.Next;
+				while(next != null)
+				{
+					sb.AppendFormat(", {0}", next.ToString());
+					next = next.Next;
+				}
+			}
+
+			return sb.ToString();
+		}
+
+		#endregion
+
+
+		#region IEnumerator<T> properties
+
+		/// <summary>
+		/// The value of the current node.
+		/// </summary>
+		public T Current
+		{
+			get
+			{
+				if(CurrentNode == null)
+					throw new InvalidOperationException("No current node.");
+
+				return CurrentNode.Value;
+			}
+		}
+		#endregion
+
+		#region IEnumerator properties
+
+		/// <summary>
+		/// The value of the current node.
+		/// </summary>
+		object IEnumerator.Current
+		{
+			get
+			{
+				return Current;
+			}
+		}
+		#endregion
+		
+		#region IEnumerator methods
+		void IEnumerator.Reset()
+		{
+			_isReset = true;
+			CurrentNode = null;
+		}
+
+		public bool MoveNext()
+		{
+			if(_isReset)
+			{
+				CurrentNode = HeadNode;
+				_isReset = false;
+			}
+			else
+			{
+				CurrentNode = CurrentNode.Next;
+			}
+
+			return (CurrentNode.Next != null);
+		}
+		#endregion
+
+		#region IDisposable methods
+		public void Dispose()
+		{
+			// nothing to do here
+		}
+		#endregion
+	}
+}
---
+++ src/InterviewFun.Library/LinkedList/Node.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace InterviewFun.LinkedList
+{
+	/// <summary>
+	/// A generic node for a singly linked list.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class Node<T>
+	{
+		#region constructors
+		/// <summary>
+		/// Creates an empty node.
+		/// </summary>
+		public Node()
+		{
+		}
+
+		/// <summary>
+		/// Creates a node with a specific value.
+		/// </summary>
+		/// <param name="value">The value to initialize the node to.</param>
+		public Node(T value)
+		{
+			Value = value;
+		}
+		#endregion
+
+		#region properties
+		/// <summary>
+		/// The value represented by this node.
+		/// </summary>
+		public T Value
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// A "pointer" (or, more accurately in .NET, a "reference") to the next node in the list.
+		/// </summary>
+		public Node<T> Next
+		{
+			get;
+			set;
+		}
+		#endregion
+
+		#region object methods
+		/// <summary>
+		/// Returns the value of the node formatted as a string.
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			if(Value != null)
+				return Value.ToString();
+			else
+				return "(null)";
+		}
+		#endregion
+	}
+}
