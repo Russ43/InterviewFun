8880305c3bb33510cfde28e4a1cd154b76d099ff
Russell Froehlich <russ@company43.com>
2016-12-06 15:54:50 -0800

    added missing file BubbleSort.cs

---
+++ src/InterviewFun.Library/Sorting/BubbleSort/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace InterviewFun
+{
+	static public class BubbleSort
+	{
+		static public IList<T> Sort<T>(IList<T> list)
+			where T : IComparable<T>
+		{
+			if(list == null)
+				throw new ArgumentNullException("list");
+
+			for(int i = 0; i < list.Count; ++i)
+			{
+				for(int j = i + 1; j < list.Count; ++j)
+				{
+					if(list[i].CompareTo(list[j]) > 0)
+					{
+						// the jth element comes before the ith element, so swap
+						T temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
+				}
+			}
+
+			return list;
+		}
+	}
+}
